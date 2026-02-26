edd7d1de04d8b38cd8aad3989987fe900b2bee15
Akib <akibraza91@gmail.com>
2026-02-26 18:58:27 +0530

    updated

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Import express
 import express from 'express';
 import bodyParser from 'body-parser';
-import puppeteer from 'puppeteer';
-// import { MongoClient } from 'mongodb';
 import client from './mongodb.js';
 
 const app = express();
@@ -13,15 +11,11 @@ app.use(express.json()); // parse JSON request bodies
 // Set up body-parser middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// // MongoDB Connection
-// const uri = "mongodb://localhost:27017";
-// const client = new MongoClient(uri);
-
 // Select database and collection
 const db = client.db('sopdb');
 const users = db.collection('students');
 
-app.post('/submit', async(req, res) => {
+app.post('/submit', async (req, res) => {
   const email = req.body.email;
   const fullname = req.body.fullname;
   const age = req.body.age;
@@ -44,29 +38,23 @@ app.post('/submit', async(req, res) => {
 
   // Revert mail statement of purpose.
   var link = "https://docs.google.com/forms/d/e/1FAIpQLSdOHGp-XNvu2p06oSWY7Mtv0R81GxHH2XGaPa1gKdIXAL6msw/formResponse?&pageHistory=0";
-  link = link + "&emailAddress="+ email +"&entry.1670307221="+ fullname +"&entry.1723368668="+ age +"&entry.2014837490="+ education;
-  link = link + "&entry.2002229478="+ institute +"&entry.1364285845="+ study + "&entry.767407612="+ experience +"&entry.1569902389="+ admission;
-  link = link + "&entry.1415007109="+ program +"&entry.2110947866="+ country +"&entry.1160268939="+ goals +"&entry.1604587969="+ listening;
-  link = link + "&entry.1283128029="+ reading +"&entry.658412876="+ speaking +"&entry.1875431276="+ writing +"&entry.692259590="+ tuitionpayment;
-  link = link + "&entry.1385560828="+ tuition +"&entry.901850093="+ gicpayment + "&entry.1308557841=" + gic;
+  // link = link + "&emailAddress="+ email +"&entry.1670307221="+ fullname +"&entry.1723368668="+ age +"&entry.2014837490="+ education;
+  // link = link + "&entry.2002229478="+ institute +"&entry.1364285845="+ study + "&entry.767407612="+ experience +"&entry.1569902389="+ admission;
+  // link = link + "&entry.1415007109="+ program +"&entry.2110947866="+ country +"&entry.1160268939="+ goals +"&entry.1604587969="+ listening;
+  // link = link + "&entry.1283128029="+ reading +"&entry.658412876="+ speaking +"&entry.1875431276="+ writing +"&entry.692259590="+ tuitionpayment;
+  // link = link + "&entry.1385560828="+ tuition +"&entry.901850093="+ gicpayment + "&entry.1308557841=" + gic;
 
   async function openLink() {
-    const browser = await puppeteer.launch({ headless: true }); // Set headless to false if you want to see the browser window.
-    const page = await browser.newPage();
-
     try {
-      await client.connect();
-      console.log('MongoDB is connected.');
       await users.insertOne(req.body);
 
-      const urlToOpen = 'link'; // Replace with your desired URL.
-
-      await page.goto(urlToOpen);
-      console.log(`Opened the URL: ${urlToOpen}`);
+      await fetch(link, {
+        method: "POST"
+      });
+      // res.send("Success");
     } catch (error) {
-      console.error('Error: ', error);
-    } finally {
-      await browser.close();
+      console.error(error);
+      // res.status(500).send("Error");
     }
   }
 
